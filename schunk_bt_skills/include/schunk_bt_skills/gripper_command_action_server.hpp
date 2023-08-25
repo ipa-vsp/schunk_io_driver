833ef449ff18b8c1d0bff1a1ef791b6ddc46900d
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2023-08-25 16:02:46 +0200

    init gripper skills

---
+++ schunk_bt_skills/include/schunk_bt_skills/gripper_command_action_server.hpp
@@ -0,0 +1,34 @@
+#ifndef GRIPPER_COMMAND_ACTION_SERVER_HPP_
+#define GRIPPER_COMMAND_ACTION_SERVER_HPP_
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "schunk_command_interface/action/gripper_command.hpp"
+#include "man2_skill_server_core/skill_action_server_lifecycle_core.hpp"
+
+namespace gripper_command_skill
+{
+    class GripperCommandActionServer : public ros2_skill_server_core::SkillActionServerLifecycleCore<
+        schunk_command_interface::action::GripperCommand>
+    {
+        public:
+            using ActionT = schunk_command_interface::action::GripperCommand;
+            explicit GripperCommandActionServer(const std::string action_name, const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+            ~GripperCommandActionServer() override = default;
+
+            nav2_util::CallbackReturn on_configure(const rclcpp_lifecycle::State & state) override;
+            nav2_util::CallbackReturn on_cleanup(const rclcpp_lifecycle::State & state) override;
+
+            void execution() override;
+
+        private:
+            std::string action_name_;
+            rclcpp::CallbackGroup::SharedPtr callback_group_;
+    };
+}
+
+#endif  // GRIPPER_COMMAND_ACTION_SERVER_HPP_
