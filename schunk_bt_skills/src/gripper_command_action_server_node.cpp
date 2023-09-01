74494292dd06d62044a5bcfe38f2883fcea7ea8b
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2023-09-01 15:10:38 +0200

    pre-commit update

--- schunk_bt_skills/src/gripper_command_action_server_node.cpp
+++ schunk_bt_skills/src/gripper_command_action_server_node.cpp
@@ -1,17 +1,37 @@
+// Copyright 2023 Vishnuprasad Prachandabhanu
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
 #include <memory>
 
-#include "schunk_bt_skills/gripper_command_action_server.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "schunk_bt_skills/gripper_command_action_server.hpp"
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
-  auto action_name = "gripper_command";
-  auto node_option = rclcpp::NodeOptions();
-  auto node =
-    std::make_shared<gripper_command_skill::GripperCommandActionServer>(action_name, node_option);
-  rclcpp::spin(node->get_node_base_interface());
-  rclcpp::shutdown();
+    rclcpp::init(argc, argv);
+    auto action_name = "gripper_command";
+    auto node_option = rclcpp::NodeOptions();
+    auto node = std::make_shared<gripper_command_skill::GripperCommandActionServer>(action_name, node_option);
+    rclcpp::spin(node->get_node_base_interface());
+    rclcpp::shutdown();
 
-  return 0;
+    return 0;
 }
